0be7c1ff45a8ed2556593a8ea19d3c4d7dd7bd6e
jirol9xa <89146943+jirol9xa@users.noreply.github.com>
2021-08-18 17:58:25 +0300

    Update Source.c

--- Source.c
+++ Source.c
@@ -1,4 +1,4 @@
-#include <stdio.h> /* s_gets()*/
+#include <stdio.h>
 #include <math.h>
 void vvod(double *a, double*b, double*c); // для ввода данных
 void eatline(); // для отбрасывания отсальной части строки, если данные будут введены с мусором
@@ -7,13 +7,13 @@ void a_0(double* b, double* c); // если а == 0
 void b_0(double* a, double* c); // если b == 0
 void c_0(double* a, double* b); // если c == 0
 void classic(double*, double*, double*); // решение через дискриминант
-double dabs(double a, double b); // abs для типа double
+double dabs(double a, double b);
 const double zero = 1e-20;
 int main(void) {
 	double a, b, c;
 	printf("Privet! Eta programma reshaet uravnenia vida a*x^2 + b*x + c = 0 \n");
 	vvod(&a, &b, &c);
-	printf("Uravnenie imeet vid: %lf*x^2 + %lf*x + %lf = 0 \n", a, b, c);
+	printf("Uravnenie imeet vid: %lg*x^2 + %lg*x + %lg = 0 \n", a, b, c);
 	if (dabs(a, 0) < zero) {
 		a_0(&b, &c);
 	}
@@ -64,11 +64,11 @@ void a_0(double* b, double* c) {
 			printf("Uravnenie imeet odin koren' x = 0 \n");
 		}
 	}
-	else if (dabs(*b, 0) < zero) { //ñëåäîâàòåëüíî ñ != 0
+	else if (dabs(*b, 0) < zero) { //следовательно с != 0
 		printf("Reshenii net \n");
 	}
 	else {
-		printf("Uravnenie imeet odno reshenie x = %lf \n", - *c / *b);
+		printf("Uravnenie imeet odno reshenie x = %lg \n", - *c / *b);
 	}
 }
 
@@ -82,14 +82,14 @@ void b_0(double* a, double* c) {
 			printf("Uravnenie imeet odno reshenie x = 0 \n");
 		}
 	}
-	else if (dabs(*a, 0) < zero) { // ñëåäîâàëüåíî ñ != 0
+	else if (dabs(*a, 0) < zero) { // следовальено с != 0
 		printf("Reshenii net \n ");
 	}
 	else if ((*a * *c) > 0) {
 		printf("Reshenii net \n ");
 	}
 	else {
-		printf("Uravnenie imeet dva reshenia x1 = %lf, x2 = %lf \n ", +sqrt(- *c / *a), -sqrt(- *c / *a));
+		printf("Uravnenie imeet dva reshenia x1 = %lg, x2 = %lg \n ", +sqrt(- *c / *a), -sqrt(- *c / *a));
 	}
 }
 
@@ -103,11 +103,11 @@ void c_0(double* a, double* b) {
 			printf("Uravnenie immet odno reshenie x = 0 \n");
 		}
 	}
-	else if (dabs(*b, 0) < zero) { // ñëåäîâàòåëüíî à != 0
+	else if (dabs(*b, 0) < zero) { // следовательно а != 0
 		printf("Uravnenie imeet odno reshenie x = 0 \n");
 	}
 	else {
-		printf("Uravnenie imeet dva reshenia x1 = 0, x2 = %lf \n ", - *b / *a);
+		printf("Uravnenie imeet dva reshenia x1 = 0, x2 = %lg \n ", - *b / *a);
 	}
 }
 
@@ -118,10 +118,10 @@ void classic(double* a, double* b, double* c) {
 		printf("Uravnenie ne imeet reshenii \n");
 	}
 	else if (dabs(diskr, 0) < zero) {
-		printf("Uravnenie imeet odno reshenie x = %lf \n", -(*b) / (2 * (*a)));
+		printf("Uravnenie imeet odno reshenie x = %lg \n", -(*b) / (2 * (*a)));
 	}
 	else {
-		printf("Uravnenie imeet dva reshenia x1 = %lf, x2 = %lf \n", (-(*b) - sqrt(diskr)) / (2 * (*a)), (-(*b) + sqrt(diskr)) / (2 * (*a)));
+		printf("Uravnenie imeet dva reshenia x1 = %lg, x2 = %lg \n", (-(*b) - sqrt(diskr)) / (2 * (*a)), (-(*b) + sqrt(diskr)) / (2 * (*a)));
 	}
 }
 
