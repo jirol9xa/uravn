1c2446a698f9f518924a6822aaa69b7b1ceac9f6
jirol9xa <89146943+jirol9xa@users.noreply.github.com>
2021-08-19 17:36:36 +0300

    Add files via upload

---
+++ functions.h
@@ -0,0 +1,23 @@
+// объ€влени€ функций
+struct Answers {
+	int amount;
+	double roots[2];
+};
+
+
+struct Coefficients {
+	double a;
+	double b;
+	double c;
+};
+
+
+void input(struct Coefficients*); // дл€ ввода данных
+void eatline(); // дл€ отбрасывани€ отсальной части строки, если данные будут введены с мусором
+void wrong_input(); // на случай неправильного ввода
+void solve_equation(struct Answers* ans, struct Coefficients* Coef); // решение уравнени€
+void linear_equation(struct Answers* ans, struct Coefficients* Coef); // если а == 0
+void the_quadratic_equation(struct Answers* ans, struct Coefficients* Coef); // решение через дискриминант
+void print_answers(struct Answers*);
+
+const double zero = 1e-20;
