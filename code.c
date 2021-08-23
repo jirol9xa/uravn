3a805c47f1a90e293d77ba2ae2a774070da44ba3
jirol9xa <89146943+jirol9xa@users.noreply.github.com>
2021-08-23 18:15:59 +0300

    Update code.c

--- code.c
+++ code.c
@@ -9,8 +9,8 @@
     Данная структура сохраняет количество корней в переменную amount и сами корни в массив roots
 */
 struct Answers {
-	int amount;
-	double roots[2];
+    int amount;
+    double roots[2];
 };
 
 
@@ -20,9 +20,9 @@ struct Answers {
     Даннас структура сохраняет значения коэффициентов a,b,c в переменные a,b и с соответсвенно
 */
 struct Coefficients {
-	double a;
-	double b;
-	double c;
+    double a;
+    double b;
+    double c;
 };
 
 
@@ -102,36 +102,36 @@ const double zero = 1e-20;
 
 
 int main(void) {
-	struct Answers ans = { 0,
-						  {0, 0} }; // первый элемент является числом решений
-	// второй и третьий равны значениям х, если они существуют
-	struct Coefficients Coef = { 0, 0, 0 };
-	printf("Hi! This program solves quadratic equations a*x^2 + b*x + c = 0 \n");
-	input(&Coef);
-	printf("Quadratic equation: %lg*x^2 + %lg*x + %lg = 0 \n", Coef.a, Coef.b, Coef.c);
-	solve_equation(&ans, &Coef);
-	print_answers(&ans);
-	printf("The equation is solved :)");
-	return 0;
+    struct Answers ans = { 0,
+                          {0, 0} }; // первый элемент является числом решений
+    // второй и третьий равны значениям х, если они существуют
+    struct Coefficients Coef = { 0, 0, 0 };
+    printf("Hi! This program solves quadratic equations a*x^2 + b*x + c = 0 \n");
+    input(&Coef);
+    printf("Quadratic equation: %lg*x^2 + %lg*x + %lg = 0 \n", Coef.a, Coef.b, Coef.c);
+    solve_equation(&ans, &Coef);
+    print_answers(&ans);
+    printf("The equation is solved :)");
+    return 0;
 }
 
 
 void input(struct Coefficients* Coef) {
     assert(Coef != NULL);
-	printf("Now enter the values a, b, c \n");
-	printf("enter a \n");
-	while(scanf("%lf", &(Coef->a)) !=1){
+    printf("Now enter the values a, b, c \n");
+    printf("enter a \n");
+    while(scanf("%lf", &(Coef->a)) !=1){
         wrong_input();
         eatline();
     }
-	printf("enter b \n");
-	while(scanf("%lf", &(Coef->b)) !=1){
+    printf("enter b \n");
+    while(scanf("%lf", &(Coef->b)) !=1){
         wrong_input();
         eatline();
     }
-	eatline();
-	printf("enter c \n");
-	while(scanf("%lf", &(Coef->c)) !=1){
+    eatline();
+    printf("enter c \n");
+    while(scanf("%lf", &(Coef->c)) !=1){
         wrong_input();
         eatline();
     }
@@ -139,79 +139,79 @@ void input(struct Coefficients* Coef) {
 
 
 void eatline() {
-	while (getchar() != '\n') {
-		continue;
-	}
+    while (getchar() != '\n') {
+        continue;
+    }
 }
 
 
 void wrong_input() {
-	printf("Just enter a number \n");
+    printf("Just enter a number \n");
 }
 
 
 void solve_equation(struct Answers* ans, struct Coefficients* Coef) {
     assert(Coef != NULL);
     assert(ans != NULL);
-	if (fabs(Coef->a) < zero) {
-		linear_equation(ans, Coef);
-	}
-	else {
-		the_quadratic_equation(ans, Coef);
-	}
+    if (fabs(Coef->a) < zero) {
+        linear_equation(ans, Coef);
+    }
+    else {
+        the_quadratic_equation(ans, Coef);
+    }
 }
 
 
 void linear_equation(struct Answers* ans, struct Coefficients* Coef) {
     assert(Coef != NULL);
     assert(ans != NULL);
-	if (fabs((*Coef).b) < zero) {
-		if (fabs((*Coef).c) < zero) {
-			(*ans).amount = 3;
-		}
-		else {
-			(*ans).amount = 0;
-		}
-	}
-	else {
-		(*ans).amount = 1;
-		(*ans).roots[0] = -(*Coef).c / (*Coef).b;
-	}
+    if (fabs((*Coef).b) < zero) {
+        if (fabs((*Coef).c) < zero) {
+            (*ans).amount = 3;
+        }
+        else {
+            (*ans).amount = 0;
+        }
+    }
+    else {
+        (*ans).amount = 1;
+        (*ans).roots[0] = -(*Coef).c / (*Coef).b;
+    }
 }
 
 
 void the_quadratic_equation(struct Answers* ans, struct Coefficients* Coef) {
     assert(Coef != NULL);
     assert(ans != NULL);
-	double diskr = Coef->b * Coef->b - 4 * Coef->a * Coef->c;
-	if (diskr < 0) {
-		ans->amount = 0;
-	}
-	else if (fabs(diskr - 0) < zero) {
-		ans->amount = 1;
-		ans->roots[0] = -(Coef->b) / (2 * (Coef->a));
-	}
-	else {
-		ans->amount = 2;
-		ans->roots[0] = (-(Coef->b) - sqrt(diskr)) / (2 * (Coef->a));
-		ans->roots[1] = (-(Coef->b) + sqrt(diskr)) / (2 * (Coef->a));
-	}
+    double diskr = Coef->b * Coef->b - 4 * Coef->a * Coef->c;
+    if (diskr < 0) {
+        ans->amount = 0;
+    }
+    else if (fabs(diskr - 0) < zero) {
+        ans->amount = 1;
+        ans->roots[0] = -(Coef->b) / (2 * (Coef->a));
+    }
+    else {
+        ans->amount = 2;
+        ans->roots[0] = (-(Coef->b) - sqrt(diskr)) / (2 * (Coef->a));
+        ans->roots[1] = (-(Coef->b) + sqrt(diskr)) / (2 * (Coef->a));
+    }
 }
 
 void print_answers(struct Answers* ans) {
     assert(ans != NULL);
-	switch (ans->amount) {
-	case 0:
-		printf("The equation has no solutions \n");
-		break;
-	case 1:
-		printf("The equation has one solution x = %lg \n", ans->roots[0]);
-		break;
-	case 2:
-		printf("The equation has one solution x1 = % lg   x2 = %lg \n", ans->roots[0], ans->roots[1]);
-		break;
-	default:
-		printf("The equation has an infinite number of solutions \n");
+    switch (ans->amount) {
+    case 0:
+        printf("The equation has no solutions \n");
+        break;
+    case 1:
+        printf("The equation has one solution x = %lg \n", ans->roots[0]);
+        break;
+    case 2:
+        printf("The equation has one solution x1 = % lg   x2 = %lg \n", ans->roots[0], ans->roots[1]);
+        break;
+    default:
+        printf("The equation has an infinite number of solutions \n");
 
-	}
+    }
 }
